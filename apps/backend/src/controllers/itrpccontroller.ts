bd3d8ad010183fa8fefbb55667d7075f1a6e9368
Nick Sharp <nick.sharp@lego.com>
2024-05-07 14:05:09 +0100

    changing client generation scripes

--- apps/backend/src/controllers/itrpccontroller.ts
+++
@@ -1,6 +0,0 @@
-// import { CreateRouterInner } from '@trpc/server';
-// import { RootConfig } from '@trpc/server/src';
-
-// export interface ITrpcController {
-//   router: CreateRouterInner<RootConfig<any>, any>;
-// }
